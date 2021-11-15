efe8432e3cea85a9e7979cabd96d1bda54c0b4fd
Santiago <santimartinez44093@gmail.com>
2021-11-15 03:07:46 -0300

    H4

--- Homework4/src/com/solvd/homework4/filescan/FileScan.java
+++ Homework4/src/com/solvd/homework4/filescan/FileScan.java
@@ -3,14 +3,23 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Logger;
 
 public class FileScan {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    private final static Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+    public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
+        } catch (FileNotFoundException e) {
+            LOGGER.warning(e.toString());
+        } catch (IOException e) {
+            LOGGER.warning(e.toString());
         }
     }
 }
+
